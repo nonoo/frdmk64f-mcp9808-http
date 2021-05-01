dac3a111380543b150c03d5a03d711d6a13f12c3
Nonoo <nonoo@nonoo.hu>
2021-05-01 18:11:43 +0200

    Use MCP9808 for temp read

--- source/main.c
+++ source/main.c
@@ -66,7 +66,7 @@
 #define I2C_MASTER_BASEADDR  I2C0
 #define I2C_MASTER_CLK_SRC   I2C0_CLK_SRC
 #define I2C_MASTER_CLK_FREQ  CLOCK_GetFreq(I2C0_CLK_SRC)
-#define I2C_MASTER_SLAVE_ADR 0x1d
+#define I2C_MASTER_SLAVE_ADR 0x18
 
 #define TEMP_INVALID -127
 
@@ -116,23 +116,35 @@ static int temp_reg_read(int regaddr) {
     i2c_master_transfer.subaddress     = regaddr;
     i2c_master_transfer.subaddressSize = 1;
     i2c_master_transfer.data           = rxbuf;
-    i2c_master_transfer.dataSize       = 1;
+    i2c_master_transfer.dataSize       = 2;
     i2c_master_transfer.flags          = kI2C_TransferDefaultFlag;
 
     I2C_MasterTransferBlocking(I2C_MASTER_BASEADDR, &i2c_master_transfer);
 
-    return rxbuf[0];
+    return (rxbuf[0] << 8) | rxbuf[1];
 }
 
 static double temp_read(void) {
-	int vendor_id = temp_reg_read(0x0d);
+	int temp_reg_val;
+	int vendor_id = temp_reg_read(0x06);
+	double celsius;
 
-	if (vendor_id != 0xc7) {
+	if (vendor_id != 0x54) {
 		PRINTF("temp-read: invalid vendor id %d\n", vendor_id);
 		return TEMP_INVALID;
 	}
 
-	return 12.34;
+	temp_reg_val = temp_reg_read(0x05);
+
+	temp_reg_val &= 0x1fff;
+
+	if (temp_reg_val & 0x1000) {
+		temp_reg_val &= 0x0f00;
+		celsius = 256 - (((double)(temp_reg_val >> 8))*16 + ((double)(temp_reg_val & 0xff))/16);
+	} else
+		celsius = ((double)(temp_reg_val >> 8))*16 + ((double)(temp_reg_val & 0xff))/16;
+
+	return celsius;
 }
 
 static void temp_handle_success(void) {
@@ -166,7 +178,7 @@ static void temp_thread(void *arg) {
 	httpcln_result_t http_res;
 	bool is_dhcp_up;
 	double temp;
-	char tmp[10];
+	char temp_str[10];
 	int f_int;
 	char s_frac[5];
 	int i;
@@ -203,13 +215,20 @@ static void temp_thread(void *arg) {
 
         float_explode(temp, &f_int, s_frac, sizeof(s_frac));
         if (temp < 0)
-        	i = snprintf(url, sizeof(url), URL_BASE "-%u.%s", abs(f_int), s_frac);
+        	i = snprintf(temp_str, sizeof(temp_str), "-%u.%s", abs(f_int), s_frac);
         else
-        	i = snprintf(url, sizeof(url), URL_BASE "%u.%s", f_int, s_frac);
+        	i = snprintf(temp_str, sizeof(temp_str), "%u.%s", f_int, s_frac);
+
+        temp_str[sizeof(temp_str)-1] = 0;
 
         // Trimming zeroes from the end.
-        while (url[--i] == '0')
-        	url[i] = 0;
+        while (temp_str[--i] == '0' && i > 0)
+        	temp_str[i] = 0;
+
+        PRINTF("temp: %s\n", temp_str);
+
+        snprintf(url, sizeof(url), URL_BASE "%s", temp_str);
+        url[sizeof(url)-1] = 0;
 
         memset(&httpcln_params, 0, sizeof(httpcln_params_t));
         httpcln_params.max_resp_content_length = 128;
